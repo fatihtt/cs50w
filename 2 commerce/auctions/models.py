c7e35970d246a50755fea20869a9a049b95d59fb
Fatih Tataroğlu <37470705+fatihtt@users.noreply.github.com>
2023-04-24 14:33:33 +0300

    commerce started

---
+++ 2 commerce/auctions/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Category(models.Model):
+      name = models.CharField(max_length=50)
+
+class Listing(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=15, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+            return f"{self.id}: {self.title}"
+    
+class Bid(models.Model):
+     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     price = models.DecimalField(max_digits=15, decimal_places=2)
+     time = models.DateTimeField(auto_now_add=True, blank=True)
+     
+     def __str__(self):
+            return f"{self.id}: {self.listing.title} : {self.price} : {self.time}"
+     
+class Comment(models.Model):
+      listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+      user = models.ForeignKey(User, on_delete=models.CASCADE)
+      message = models.TextField()
+      time = models.DateTimeField(auto_now_add=True, blank=True)
+
+      def __str__(self):
+            return f"{self.id}: {self.listing.title} : {self.user.username} : {self.message}"
+      
+class Watch(models.Model):
+      listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+      user = models.ForeignKey(User, on_delete=models.CASCADE)
+      time = models.DateTimeField(auto_now_add=True, blank=True)
+
+      def __str__(self):
+            return f"{self.id}: {self.listing.title} : {self.user.username} : {self.time}"
