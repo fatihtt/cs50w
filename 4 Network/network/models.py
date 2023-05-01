63a9eb1ce333a89e87b91fe11ceead4600fa9bb4
Fatih Tataroglu <fthttrgl@gmail.com>
2023-05-02 00:19:32 +0300

    models created

--- 4 Network/network/models.py
+++ 4 Network/network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "text": self.text,
+            "time": self.time
+        }
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+class Fallowing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="fallowing_user")
+    fallower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="fallower_user")
+    time = models.DateTimeField(auto_now_add=True)
